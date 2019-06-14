000142fe76c5cde12d99d70a543205306ac3bdca
Anindito Bhagawanta <anindito.87bagas@gmail.com>
2019-06-14 11:06:33 +0700

    Update controller test

--- src/test/java/siakng/controller/SiakNgControllerTest.java
+++ src/test/java/siakng/controller/SiakNgControllerTest.java
@@ -1,5 +1,6 @@
 package siakng.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -10,6 +11,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -20,12 +22,14 @@ import siakng.service.SiakNgService;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.assertj.core.internal.bytebuddy.matcher.ElementMatchers.is;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.internal.verification.VerificationModeFactory.times;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
 @RunWith(SpringRunner.class)
@@ -49,7 +53,6 @@ public class SiakNgControllerTest {
         Course c2 = new Course(2L, "DDP2", 4);
 
         List<Course> courseList = Arrays.asList(c1, c2);
-        given(siakNgService.getCourses()).willReturn(courseList);
         Mockito.when(siakNgService.getCourses()).thenReturn(courseList);
 
         this.mockMvc.perform(get("/academic/"))
@@ -59,6 +62,75 @@ public class SiakNgControllerTest {
                 .andExpect(jsonPath("$[0].name").value("Statprob"))
                 .andExpect(jsonPath("$[1].id").value("2"))
                 .andExpect(jsonPath("$[1].name").value("DDP2"));
+
+        verify(siakNgService, times(1)).getCourses();
+        verifyNoMoreInteractions(siakNgService);
+    }
+
+    @Test
+    public void getCourse() throws Exception {
+        Course c1 = new Course(1L, "Statprob", 3);
+
+        Mockito.when(siakNgService.getCourse(1L)).thenReturn(c1);
+
+        this.mockMvc.perform(get("/academic/1"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value("1"))
+                .andExpect(jsonPath("$.name").value("Statprob"));
+
+        verify(siakNgService, times(1)).getCourse(1L);
+        verifyNoMoreInteractions(siakNgService);
+    }
+
+    @Test
+    public void addCourse() throws Exception {
+        Course c1 = new Course(1L, "Statprob", 3);
+        ObjectMapper objectMapper = new ObjectMapper();
+        String json = objectMapper.writeValueAsString(c1);
+
+        Mockito.when(siakNgService.addCourse(any(Course.class))).thenReturn(c1);
+
+        this.mockMvc.perform(post("/academic/add")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json)
+                .characterEncoding("utf-8"))
+                .andDo(print())
+                .andExpect(status().isOk());
+
+        verify(siakNgService, times(1)).addCourse("Statprob", 3);
+        verifyNoMoreInteractions(siakNgService);
+                /*.andExpect(jsonPath("$.id").value("1"))
+                .andExpect(jsonPath("$.name").value("Statprob"));*/
+    }
+
+    @Test
+    public void deleteCourse() throws Exception {
+        Course c1 = new Course(1L, "Statprob", 3);
+        ObjectMapper objectMapper = new ObjectMapper();
+        String json = objectMapper.writeValueAsString(c1);
+
+        Mockito.when(siakNgService.addCourse(any(Course.class))).thenReturn(c1);
+
+        this.mockMvc.perform(post("/academic/add")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json)
+                .characterEncoding("utf-8"))
+                .andDo(print())
+                .andExpect(status().isOk());
+
+        verify(siakNgService, times(1)).addCourse("Statprob", 3);
+
+        Mockito.when(siakNgService.deleteCourse(1L)).thenReturn(c1);
+
+        this.mockMvc.perform(delete("/academic/delete/1"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value("1"))
+                .andExpect(jsonPath("$.name").value("Statprob"));
+
+        verify(siakNgService, times(1)).deleteCourse(1L);
+        verifyNoMoreInteractions(siakNgService);
     }
 
 
